aaaaf85a421bbb78da0b4817af7cd003fa778eba
Vic428-human <z0983195379a@gmail.com>
2026-03-12 03:14:13 +0800

    install @workspace/backend + convex in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,10 @@
-import { Input } from "@workspace/ui/components/input"
-import { add } from "@workspace/math/add"
+import { useQuery } from 'convex/react'
 
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Hello apps/web</h1>
-          <p>{add(2, 2)}</p>
-        </div>
-        <Input/>
-      </div>
+      <p>app/web</p>
     </div>
   )
 }
