2c6f5f3653c20424993f8d17236490a95639d6f3
Vic428-human <z0983195379a@gmail.com>
2026-03-12 15:12:29 +0800

    convex setting for provider

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { useQuery } from 'convex/react'
 // @workspace/backend 這個路徑開頭，是因為前端專案的 tsconfig.json 已經有設定過 paths 自己定義路徑名
-// 換句話說，會來如果要使用共用 packages時，都可以先在 tsconfig.json 定義好指定的路徑名稱 再進行引用
-import { api } from '@workspace/backend/convex/_generated/api' // 支援 IDE 提示：VSCode 等編輯器能正確跳轉到來源檔案。
+// 換句話說，未來使用共用的 packages 時，都可以先在 tsconfig.json 定義好指定的路徑名稱 再進行引用，
+// 只是目前引用的是 convex 的後端功能
+import { api } from '@workspace/backend/_generated/api' // 支援 IDE 提示：VSCode 等編輯器能正確跳轉到來源檔案。
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
